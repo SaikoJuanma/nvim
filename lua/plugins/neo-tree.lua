440a6bef14dcad88b9aae6e51d1230a7059d647d
Juan Manuel Schilling <juan.manuel.schilling@innovex.cl>
2025-04-11 13:03:21 -0400

    hidden files now can be seen on neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return{
+return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -6,5 +6,22 @@ return{
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, -- only works on Windows for hidden files/directories
+          },
+          window = {
+            mappings = {
+              ["H"] = "toggle_hidden",
+            }
+          }
+        }
+      })
+    end
 }
