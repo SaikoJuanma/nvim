207eab492e2e128e753b6f1d158b06eae4aff8dd
Juan Manuel Schilling <juan.manuel.schilling@innovex.cl>
2026-02-24 09:16:34 -0300

    support for rust formating

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,22 @@
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^5',
+  lazy = false,
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Rust Go to Definition" })
+          vim.keymap.set("n", "K",  vim.lsp.buf.hover,      { buffer = bufnr, desc = "Rust Hover Actions" })
+        end,
+        default_settings = {
+          ['rust-analyzer'] = {
+            checkOnSave = {
+              command = "clippy",
+            },
+          },
+        },
+      },
+    }
+  end,
+}
